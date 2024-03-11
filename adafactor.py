dd5c1d86e0b64640cb00efb77ff1cda034f7e443
duanjunwen <935724073@qq.com>
2024-03-11 16:10:41 +0800

    [feature](ual) modify test case; remove model, backward step;

--- adafactor.py
+++ adafactor.py
@@ -193,6 +193,7 @@ class Adafactor(Optimizer):
         for group in self.param_groups:
             # update weight & bias
             for p in group["params"]:
+                # print(f"base p.grad {p.grad}\n")
                 if p.grad is None:
                     continue
                 """
@@ -961,6 +962,227 @@ class AdafactorTPv02(Optimizer):
             "warmup_init": warmup_init,
         }
         super().__init__(params, defaults)
+        self.localRank = int(os.environ['LOCAL_RANK'])  # 当前GPU id
+
+    @staticmethod
+    def _get_lr(param_group, param_state):
+        rel_step_sz = param_group["lr"]
+        if param_group["relative_step"]:
+            min_step = 1e-6 * param_state["step"] if param_group["warmup_init"] else 1e-2
+            rel_step_sz = min(min_step, 1.0 / math.sqrt(param_state["step"]))
+        param_scale = 1.0
+        if param_group["scale_parameter"]:
+            param_scale = max(param_group["eps"][1], param_state["RMS"])
+        return param_scale * rel_step_sz
+
+    @staticmethod
+    def _get_options(param_group, param_shape):
+        # print(f"param_shape {len(param_shape)}")
+        factored = len(param_shape) >= 2
+        use_first_moment = param_group["beta1"] is not None
+        return factored, use_first_moment
+
+    @staticmethod         
+    def _rms(tensor):
+        return tensor.norm(2) / (tensor.numel() ** 0.5)
+
+    @staticmethod
+    def _approx_sq_grad(exp_avg_sq_row, exp_avg_sq_col):
+        # copy from fairseq's adafactor implementation:
+        # https://github.com/huggingface/transformers/blob/8395f14de6068012787d83989c3627c3df6a252b/src/transformers/optimization.py#L505
+        
+        # print(f"Row col shape Before {exp_avg_sq_row.shape, exp_avg_sq_col.shape}")
+        # exp_avg_sq_row shape [8]
+        # exp_avg_sq_col shape [8]
+        r_factor = (exp_avg_sq_row / exp_avg_sq_row.mean(dim=-1, keepdim=True)).rsqrt_().unsqueeze(-1)
+        c_factor = exp_avg_sq_col.unsqueeze(-2).rsqrt()
+        # r_factor shape [8,1]
+        # c_factor shape [1,8]
+        # print(f"Row col shape After {r_factor.shape, c_factor.shape}")
+        return torch.mul(r_factor, c_factor)
+
+        
+    @torch.no_grad()
+    def step(self, closure=None):
+        """
+        Performs a single optimization step
+
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            loss = closure()
+        # print(f"param_groups:\n {list(self.param_groups)}")
+        
+        # print(f"Self Param TPsize, localRank, worldSize:{self.tensor_parallel_size, self.localRank, self.worldSize}")
+        """
+        param_groups: Dict
+        {
+            "params":[weight, bias]
+            "lr"
+            "eps"
+            "clip_threshold"
+            "decay_rate"
+            "beta1"
+            "weight_decay"
+            "scale_parameter"
+            "relative_step"
+            "warmup_init"
+        }
+        """
+        # print(f"self.param_groups", self.param_groups,"\n")
+        for group in self.param_groups:
+            # update weight & bias
+            for p in group["params"]:
+                # print(f"p.grad {p.grad}\n")
+                if p.grad is None:
+                    continue
+                """
+                # grad shape is same as weigh / bias
+                """
+                grad = p.grad.to(self.localRank)
+                # print(f"grad {p.grad}")
+                if grad.dtype in {torch.float16, torch.bfloat16}:
+                    grad = grad.float()
+                if grad.is_sparse:
+                    raise RuntimeError("Adafactor does not support sparse gradients.")
+                
+                """
+                p (group["params"]) is weight
+                state 
+                {'step', 
+                'exp_avg_sq_row', 
+                'exp_avg_sq_col', 
+                'RMS'
+                }
+                
+                p (group["params"]) is bias
+                state 
+                {'step', 
+                'exp_avg_sq', 
+                'RMS'
+                }
+                """
+                state = self.state[p]
+                # print(f"state {list(state)}")
+                grad_shape = grad.shape
+                # print(f"grad_shape {grad_shape}")
+
+                factored, use_first_moment = self._get_options(group, grad_shape)
+                # print(f"factor {factored}, use_first_moment { use_first_moment}")
+                # State Initialization
+                if len(state) == 0:
+                    state["step"] = 0
+                    if use_first_moment:
+                        # Exponential moving average of gradient values
+                        state["exp_avg"] = torch.zeros_like(grad)
+                    if factored:
+                        state["exp_avg_sq_row"] = torch.zeros(grad_shape[:-1]).to(grad)
+                        state["exp_avg_sq_col"] = torch.zeros(grad_shape[:-2] + grad_shape[-1:]).to(grad)
+                    else:
+                        state["exp_avg_sq"] = torch.zeros_like(grad)
+
+                    state["RMS"] = 0
+                else:
+                    if use_first_moment:
+                        state["exp_avg"] = state["exp_avg"].to(grad)
+                    if factored:
+                        state["exp_avg_sq_row"] = state["exp_avg_sq_row"].to(grad)
+                        state["exp_avg_sq_col"] = state["exp_avg_sq_col"].to(grad)
+                    else:
+                        state["exp_avg_sq"] = state["exp_avg_sq"].to(grad)
+
+                p_data_fp32 = p.to(self.localRank)
+                if p.dtype in {torch.float16, torch.bfloat16}:
+                    p_data_fp32 = p_data_fp32.float()
+                
+                state["step"] += 1
+                state["RMS"] = self._rms(p_data_fp32)
+                lr = self._get_lr(group, state)
+                
+                # 参数Beta 2
+                beta2t = 1.0 - math.pow(state["step"], group["decay_rate"])
+                # print(f"{state['step'], group['decay_rate'], math.pow(state['step'], group['decay_rate'])}")
+                
+                update = (grad**2) + group["eps"][0]
+                
+                if factored:  
+                    # 若使用adafactor
+                    # print(f"factor\n")
+                    exp_avg_sq_row = state["exp_avg_sq_row"]
+                    exp_avg_sq_col = state["exp_avg_sq_col"]
+                    
+                    # (Line No.5)计算行指数平均
+                    # print(f"------------------------shape {update.mean(dim=-1).shape}")
+                    exp_avg_sq_row.mul_(beta2t).add_(update.mean(dim=-1), alpha=(1.0 - beta2t))
+                    # (Line No.6)计算列指数平均
+                    exp_avg_sq_col.mul_(beta2t).add_(update.mean(dim=-2), alpha=(1.0 - beta2t))
+                    
+                    # (Line No.7)近似计算，提前开根号
+                    # Approximation of exponential moving average of square of gradient
+                    update = self._approx_sq_grad(exp_avg_sq_row, exp_avg_sq_col)
+                    update.mul_(grad)
+                else:
+                    # 若使用adam
+                    exp_avg_sq = state["exp_avg_sq"]
+
+                    exp_avg_sq.mul_(beta2t).add_(update, alpha=(1.0 - beta2t))
+                    update = exp_avg_sq.rsqrt().mul_(grad)
+                
+                #  (Line No.8)
+                update.div_((self._rms(update) / group["clip_threshold"]).clamp_(min=1.0))
+                update.mul_(lr)
+
+                if use_first_moment:
+                    exp_avg = state["exp_avg"]
+                    exp_avg.mul_(group["beta1"]).add_(update, alpha=(1 - group["beta1"]))
+                    update = exp_avg
+
+                if group["weight_decay"] != 0:
+                    p_data_fp32.add_(p_data_fp32, alpha=(-group["weight_decay"] * lr))
+
+                p_data_fp32.add_(-update)
+
+                if p.dtype in {torch.float16, torch.bfloat16}:
+                    p.copy_(p_data_fp32)
+
+        return loss
+
+# Adafactor Tensor parallel v0.3(zero stage 2, flatten param weight )
+class AdafactorTPv03(Optimizer):
+    def __init__(
+        self,
+        params,
+        lr=None,
+        eps=(1e-30, 1e-3),
+        clip_threshold=1.0,
+        decay_rate=-0.8,
+        beta1=None,
+        weight_decay=0.0,
+        scale_parameter=True,
+        relative_step=True,
+        warmup_init=False,
+    ):
+        # require_version("torch>=1.5.0")  # add_ with alpha
+        if lr is not None and relative_step:
+            raise ValueError("Cannot combine manual `lr` and `relative_step=True` options")
+        if warmup_init and not relative_step:
+            raise ValueError("`warmup_init=True` requires `relative_step=True`")
+
+        defaults = {
+            "lr": lr,
+            "eps": eps,
+            "clip_threshold": clip_threshold,
+            "decay_rate": decay_rate,
+            "beta1": beta1,
+            "weight_decay": weight_decay,
+            "scale_parameter": scale_parameter,
+            "relative_step": relative_step,
+            "warmup_init": warmup_init,
+        }
+        super().__init__(params, defaults)
 
     @staticmethod
     def _get_lr(param_group, param_state):
@@ -1144,4 +1366,3 @@ class AdafactorTPv02(Optimizer):
                     p.copy_(p_data_fp32)
 
         return loss
-
--- main.py
+++ main.py
@@ -30,10 +30,11 @@ def init_dist():
     torch.cuda.set_device(local_rank)
 
 def correctness_verify(tensor1: torch.Tensor, tensor2: torch.Tensor):
-    return torch.all(tensor1.eq(tensor2))
+    # return torch.all(tensor1.eq(tensor2))
+    return torch.all(tensor1.isclose(tensor2, rtol=1e-05, atol=1e-05))
     
 def error_idx(tensor1: torch.Tensor, tensor2: torch.Tensor):
-    return torch.eq(tensor1, tensor2)
+    return torch.isclose(tensor1, tensor2, rtol=1e-05, atol=1e-05)
 
 def get_time():
     torch.cuda.synchronize()
@@ -49,52 +50,28 @@ def main():
     base_start, base_end, base_runtime = 0, 0, 0
     tp_start, tp_end, tp_runtime = 0, 0, 0
     
-    # Data and model Param
-    a = 0.1
-    b = 1.0
-    error = 0.1
-    batch_size = 256
-    # Weight and bias shape; Weight [H, W], Bias 
     H, W = 4096, 4096
-    # Data
-    x = torch.arange(0.1, 0.1 + batch_size * H * 0.1, 0.1).reshape(batch_size, H).cuda(device=device)
-    y_true = a * x + b + (torch.randn(batch_size, 1).cuda(device=device) * error) # shape [batch size, H]
-    y_true = y_true.cuda(device=device)
 
     model_base = nn.Linear(H, W).to(device)  
-    model_tp = model_base.to(device)
-    model_tp_zero2 = model_base.to(device)
     # weight [H*W]
     # bias [W]
     weight, bias = model_base.weight, model_base.bias
 
+    local_weight = _split(weight)
+    local_weight = nn.Parameter(local_weight.requires_grad_(True))
+    local_bias = nn.Parameter(bias.requires_grad_(True))
     # # ==============================
-    # # Adafactor
+    # # Adafactor Base
     # # ==============================
     # optimizer_base = Adafactor(model_base.parameters(), beta1 = 0.9, weight_decay=0.1)
     # print(f"Before base weight shape {weight.shape} on device {device} {weight.data}")
     torch.cuda.synchronize()
+    
     optimizer_base = Adafactor([weight, bias], beta1 = 0.9, weight_decay=0.1)
-    loss_fn_base = nn.MSELoss()
     optimizer_base.zero_grad()
-    y_pred_base = model_base(x)
-    loss_base = loss_fn_base(y_pred_base, y_true)
-    loss_base.backward()
-    # print(f"weight.grad shape {weight.grad.shape} {weight.grad}")
-    # print(f"model.weight.grad shape {model_base.weight.grad.shape} {model_base.weight.grad}")
+    weight.grad = torch.rand_like(weight)
+    bias.grad = torch.rand_like(bias)
     optimizer_base.step()
-    # print(f"After base weight shape {weight.shape} on device {device} {weight.data}")
-    # # ==============================
-    # # Adafactor Tensor Parallel v0.0
-    # # ==============================
-    # optimizer_tp = AdafactorTP(model_tp.parameters())
-    # loss_fn_tp = loss_fn_basedddd
-
-    # optimizer_tp.zero_grad()
-    # y_pred_tp = model_tp(x)
-    # loss_tp = loss_fn_tp(y_pred_tp, y_true)
-    # loss_tp.backward()
-    # optimizer_tp.step()
     
     # ==============================
     # Adafactor Tensor Parallel v0.1
@@ -113,33 +90,17 @@ def main():
     # Adafactor Tensor Parallel v0.2
     # ==============================
     torch.cuda.synchronize()
-    # print(f"Before local_weight shape {local_weight.shape} on {device}:{local_weight}")
-    # optimizer_tp = AdafactorTPv02([local_weight, local_bias],  beta1 = 0.9,  weight_decay=0.1)
-    loss_fn_tp = loss_fn_base
-    y_pred_tp_1 = model_tp(x)
-    loss_tp_1 = loss_fn_tp(y_pred_tp_1, y_true)
-    loss_tp_1.backward()
-    # print(f"local_weight.grad {local_weight.grad}")
-    local_weight = _split(model_tp.weight)
-    local_weight = nn.Parameter(local_weight.requires_grad_(True))
-    local_bias = nn.Parameter(model_tp.bias.requires_grad_(True))
     optimizer_tp = AdafactorTPv02([local_weight, local_bias],  beta1 = 0.9,  weight_decay=0.1)
-    # print(f"local_weight.grad shape {model_tp.weight.grad.shape} {model_tp.weight.grad}")
     optimizer_tp.zero_grad()
+    local_weight.grad = _split(weight.grad)
+    local_bias.grad = bias.grad
+    # print(f"local_weight grad shape {local_weight.grad.shape}{local_weight.grad}")
     optimizer_tp.step()
     # print(f"After local_weight shape {local_weight.shape} on {device}:{local_weight}")
 
     # # ==============================
-    # # Adafactor Tensor Parallel + Zero Stage 2
+    # # Adafactor Tensor Parallel v0.3
     # # ==============================
-    # optimizer_tp_zero2 = AdafactorTpZero2(model_tp_zero2.parameters())
-    # loss_fn_tp_zero2 = loss_fn_base
-
-    # optimizer_tp_zero2.zero_grad()
-    # y_pred_tp_zero2 = model_tp_zero2(x)
-    # loss_tp_zero2 = loss_fn_tp_zero2(y_pred_tp_zero2, y_true)
-    # loss_tp_zero2.backward()
-    # optimizer_tp_zero2.step()
 
     # ==============================
     # Correctness Verify
@@ -147,54 +108,61 @@ def main():
     # torch.cuda.synchronize()
     torch.cuda.synchronize()
     gather_weight = _gather(local_weight.data)
-    # print(f"gather_weight shape {gather_weight.shape} on device {device}  {gather_weight.data}")
-    weight_correctness = correctness_verify(weight.data, gather_weight.data)
+    # print(f"weight.data shape {weight.data.shape} {weight.data}")
+    # print(f"gather_weight.data shape {gather_weight.data.shape} {gather_weight.data}")
+
+    weight_correctness = correctness_verify(weight.data, gather_weight)
     bias_correctness = correctness_verify(bias.data, local_bias.data)
-    print(f"weight correctness {weight_correctness}")
-    print(f"bias correctness {bias_correctness}")
     
-    # # ==============================
-    # # Run training epoch
-    # # ==============================
+    if weight_correctness:
+        print(f"weight correctness {weight_correctness}")
+    else:
+        weight_err_idx = error_idx(weight.data, gather_weight.data)
+        print(f"weight err idx {weight_err_idx}")
+            
+    if bias_correctness:
+        print(f"bias correctness {bias_correctness}")
+    else:
+        bias_err_idx = error_idx(bias.data, local_bias.data)
+        print(f"bias err idx {bias_err_idx}")
+
+    
+  
+
+    # ==============================
+    # Run training epoch
+    # ==============================
     niter = 10
     for i in range(0, niter):
         # Base optim
         optimizer_base.zero_grad()
-        y_pred_base = model_base(x)
-        loss_base = loss_fn_base(y_pred_base, y_true)
-        loss_base.backward()
         base_start = get_time()
         optimizer_base.step()
         base_end = get_time()
         
         # TP optim
         optimizer_tp.zero_grad()
-        y_pred_tp = model_tp(x)
-        loss_tp = loss_fn_tp(y_pred_tp, y_true)
-        loss_tp.backward()
         tp_start = get_time()
         optimizer_tp.step()
         tp_end = get_time()
         
         torch.cuda.synchronize()
-        weight_correctness = correctness_verify(list(model_base.parameters())[0].data, list(model_tp.parameters())[0].data)
-        bias_correctness = correctness_verify(list(model_base.parameters())[1].data, list(model_tp.parameters())[1].data)
-        
-        # print(f"model_base weight device {device} {list(model_base.parameters())[0].data}")
-        # print(f"model_tp weight device {device} {list(model_tp.parameters())[0].data}")
+        weight_correctness = correctness_verify(weight.data, gather_weight)
+        bias_correctness = correctness_verify(bias.data, local_bias.data)
         
         print(f"iter {i}")
         if weight_correctness:
             print(f"weight correctness {weight_correctness}")
         else:
-            weight_err_idx = error_idx(list(model_base.parameters())[0].data, list(model_tp.parameters())[0].data)
-            print(f"bias err idx {weight_err_idx}")
-            
+            weight_err_idx = error_idx(weight.data, gather_weight.data)
+            print(f"weight err idx {weight_err_idx}")
+                
         if bias_correctness:
             print(f"bias correctness {bias_correctness}")
         else:
-            bias_err_idx = error_idx(list(model_base.parameters())[1].data, list(model_tp.parameters())[1].data)
+            bias_err_idx = error_idx(bias.data, local_bias.data)
             print(f"bias err idx {bias_err_idx}")
+
         print(f"Current base avg runtime {(base_end - base_start) * 10.0**3} ms; Current tp avg runtime {(tp_end - tp_start)*10.0**3} ms")
         base_runtime += base_end - base_start
         tp_runtime += tp_end - tp_start
