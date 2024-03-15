da3c338f8c5ea1845ac23786989b9a7c40d306d1
duanjunwen <935724073@qq.com>
2024-03-15 11:13:01 +0800

    [feature](ual) remove redundant files: utils.py, initialize.py, etc; Use colossalai.tensor_dtensor API;

--- main.py
+++ main.py
@@ -1,30 +1,20 @@
 import os
 import time
+from prettytable import PrettyTable
+
 import torch
 from torch import nn
-from torch.nn.parameter import Parameter
 import torch.distributed as dist
-# import torch.optim as optim
+
+from mappings import _gather
 from adafactor import (
     Adafactor, 
     DistributedAdaFactor,
 )
-import initialize as fs_init
-from mappings import (
-    _split,
-    _gather
-)
-from prettytable import PrettyTable
+
 from colossalai.tensor.d_tensor import (
     distribute_tensor,
-    distribute_tensor_with_customization,
-    get_device_mesh,
-    get_sharding_spec,
-    init_as_dtensor,
-    init_tensor_as_customization_distributed,
-    is_customized_distributed_tensor,
-    is_distributed_tensor,
-    ShardingSpec
+    ShardingSpec,
 )
 from colossalai.device.device_mesh import DeviceMesh
 
@@ -34,58 +24,61 @@ def init_dist():
     rank = int(os.environ['RANK']) 
     local_rank = int(os.environ['LOCAL_RANK'])
     world_size = int(os.environ['WORLD_SIZE'])
-
     dist.init_process_group(world_size=world_size, rank=rank,
                             init_method="env://", backend="nccl")
-    fs_init.initialize_model_parallel(model_parallel_size_= world_size)
     torch.cuda.set_device(local_rank)
 
 def correctness_verify(tensor1: torch.Tensor, tensor2: torch.Tensor):
     return torch.all(tensor1.isclose(tensor2, rtol=1e-05, atol=1e-05, equal_nan=True))
-    # return torch.testing.assert_close(tensor1, tensor2,  rtol=1e-05, atol=1e-04, equal_nan=True)
+    # return torch.testing.assert_close(tensor1, tensor2,  rtol=1e-05, atol=1e-05, equal_nan=True)
     
 def error_idx(tensor1: torch.Tensor, tensor2: torch.Tensor):
-    return torch.isclose(tensor1, tensor2, rtol=1e-05, atol=1e-04, equal_nan=True)
+    return torch.isclose(tensor1, tensor2, rtol=1e-05, atol=1e-05, equal_nan=True)
 
 def get_time():
     torch.cuda.synchronize()
     return time.time()
 
 def main():
+    # ==============================
+    # torch distributed init
+    # ==============================
     torch.manual_seed(0)
     device = int(os.environ['LOCAL_RANK'])
     world_size = int(os.environ['WORLD_SIZE'])
+    tensor_parallel_size = world_size
     init_dist()
-    tensor_parallel_size = fs_init.get_model_parallel_world_size() 
     
-    H, W = 4, 4
+    # ==============================
+    # Param Info
+    # ==============================
+    H, W = 4096, 4096
     model_base = nn.Linear(H, W).to(device)  
     
+    # ==============================
+    # Param init
+    # ==============================
     # base param
     # layer shape [H, W], then
     # weight [W, H] [4, 2]
     # bias [W]  [4]
     weight, bias = model_base.weight, model_base.bias
     # physical_mesh_id: torch.Tensor[0,1,2,3]
-    # logical_mesh_id: 2 GPU view as (1,2); 4 GPU view as (1,4) or (2,2)
-    # device_mesh = DeviceMesh(torch.Tensor([i for i in range(world_size)]), (1, 2), init_process_group=True)
-    # sharding_spec = ShardingSpec(dim_size=weight.dim(), dim_partition_dict={weight.dim() - 1: [1]})
-    # weight_shard = distribute_tensor(weight, device_mesh, sharding_spec)
-    # print(f"device_mesh {device_mesh}")
-    # print(f"sharding_spec {sharding_spec}")
-    # print(f"weight on device {device} {weight}")
-    # print(f"weight_shard on device {device} {weight_shard}")
+    # logical_mesh_id: (DP size, TP size); WORLD SIZE = DP size, TP size; 2 GPU view as (1,2); 4 GPU view as (1,4) or (2,2)
+    device_mesh = DeviceMesh(torch.Tensor([i for i in range(world_size)]), (1, tensor_parallel_size), init_process_group=True)
+    sharding_spec = ShardingSpec(dim_size=weight.dim(), dim_partition_dict={weight.dim() - 1: [1]})
+    weight_shard = distribute_tensor(weight, device_mesh, sharding_spec)
     
     # local_weight [W*H/N] [4*1]
     # local_bias [W]  [4]
     # flatten param; TP first, then flatten ;
-    local_weight_flatten = nn.Parameter(_split(weight).clone().flatten().requires_grad_(True))
+    local_weight_flatten = nn.Parameter(weight_shard.clone().flatten().requires_grad_(True))
     local_bias_flatten = nn.Parameter(bias.clone().flatten().requires_grad_(True))
 
 
-    # # ==============================
-    # # Adafactor Base
-    # # ==============================
+    # ==============================
+    # Adafactor Base
+    # ==============================
     torch.cuda.synchronize()
     optimizer_base = Adafactor([weight, bias])
     optimizer_base.zero_grad()
@@ -94,12 +87,12 @@ def main():
     optimizer_base.step()
 
     # # ==============================
-    # # Adafactor Tensor Parallel v0.3
+    # # DistributedAdafactor
     # # ==============================
     torch.cuda.synchronize()
-    optimizer_zero2 = DistributedAdaFactor([local_weight_flatten, local_bias_flatten], param_height = W, param_width = H)
+    optimizer_zero2 = DistributedAdaFactor([local_weight_flatten, local_bias_flatten], param_height = W, param_width = H, device_mesh=device_mesh, sharding_spec=sharding_spec)
     optimizer_zero2.zero_grad()
-    local_weight_flatten.grad = _split(weight.grad).clone().flatten()
+    local_weight_flatten.grad = distribute_tensor(weight.grad, device_mesh, sharding_spec).clone().flatten()
     local_bias_flatten.grad = bias.grad.clone().flatten()
     optimizer_zero2.step()
     
@@ -109,33 +102,32 @@ def main():
     # tensor parallel & flatten view &gather data
     torch.cuda.synchronize()
     reshape_flatten_weight = local_weight_flatten.view(-1, H // tensor_parallel_size) # reshape
-    gather_flatten_weight = _gather(reshape_flatten_weight.data) # gather
+    gather_flatten_weight = _gather(reshape_flatten_weight.data, device_mesh.get_process_group(axis=1)) # gather
     weight_correctness = correctness_verify(weight.data, gather_flatten_weight)
     bias_correctness = correctness_verify(bias.data, local_bias_flatten.data)
     if weight_correctness:
-        print(f"Distributed weight correctness {weight_correctness}")
+        print(f"Distributed weight correctness Pass")
     else:
         print(f"Distributed weight inclued incorrectness value")
         weight_err_idx = error_idx(weight.data, gather_flatten_weight.data)
         print(f"Distributed weight err idx {weight_err_idx}")
     if bias_correctness:
-        print(f"Distributed bias correctness {bias_correctness}")
+        print(f"Distributed bias correctness Pass")
     else:
         print(f"Distributed bias inclued incorrectness value")
         bias_err_idx = error_idx(bias.data, local_bias_flatten.data)
         print(f"Distributed bias err idx {bias_err_idx}")
 
-    # # ==============================
-    # # Run training epoch
-    # # ==============================
-    niter = 1
-    # runtime test
+    # ==============================
+    # Runtime Test
+    # ==============================
+    niter = 50
     base_start, base_end, base_runtime = 0, 0, 0
     zero_start, zero_end, zero_runtime, zero_best_runtime = 0, 0, 0, float('inf')
     table = PrettyTable(['Version', 'weight shape', 'bias shape', 'Avg runtime(ms)',
                         'Speed Up Rate', 'Best Speed Up Rate'], float_format='.2')
     for i in range(0, niter):
-        # Base optim
+        # Base Adafactor
         optimizer_base.zero_grad()
         weight.grad = torch.rand_like(weight)
         bias.grad = torch.rand_like(bias)
@@ -143,34 +135,34 @@ def main():
         optimizer_base.step()
         base_end = get_time()
         
-        # TP+Zero2 optim
+        # Distributed Adafactor
         optimizer_zero2.zero_grad()
-        local_weight_flatten.grad = _split(weight.grad).clone().flatten()
+        local_weight_flatten.grad = distribute_tensor(weight.grad, device_mesh, sharding_spec).clone().flatten()
         local_bias_flatten.grad = bias.grad.clone().flatten()
         zero_start = get_time()
         optimizer_zero2.step()
         zero_end = get_time()
     
         reshape_flatten_weight = local_weight_flatten.view(-1, H // tensor_parallel_size) # reshape
-        gather_flatten_weight = _gather(reshape_flatten_weight.data) # gather
-        
-     
+        gather_flatten_weight = _gather(reshape_flatten_weight.data, device_mesh.get_process_group(axis=1)) # gather
+
         torch.cuda.synchronize()
         v3_weight_correctness = correctness_verify(weight.data, gather_flatten_weight)
         v3_bias_correctness = correctness_verify(bias.data, local_bias_flatten.data)
         
         
         print(f"iter {i}")
-
         if v3_weight_correctness:
-            print(f"Distributed weight correctness {v3_weight_correctness}")
+            print(f"Distributed weight correctness Pass")
         else:
+            print(f"Distributed weight inclued incorrectness value")
             weight_err_idx = error_idx(weight.data, gather_flatten_weight.data)
             print(f"Distributed weight err idx {weight_err_idx}")
                 
         if v3_bias_correctness:
-            print(f"Distributed bias correctness {v3_bias_correctness}")
+            print(f"Distributed bias correctness Pass")
         else:
+            print(f"Distributed bias inclued incorrectness value")
             bias_err_idx = error_idx(bias.data, local_bias_flatten.data)
             print(f"Distributed bias err idx {bias_err_idx}")
 
--- mappings.py
+++ mappings.py
@@ -1,56 +1,9 @@
-from typing import Any
-
 import torch
-import torch.distributed as dist
-from initialize import get_model_parallel_group
-from utils import split_tensor_along_last_dim
-
-# torch.Tensor should be a cuda tensor
-def _reduce(ctx: Any, input_: torch.Tensor) -> torch.Tensor:
-    """All-reduce the the input tensor across model parallel group."""
-    group = get_model_parallel_group()
-
-    if ctx:
-        ctx.mark_dirty(input_)
-
-    # Bypass the function if we are using only 1 GPU.
-    if torch.distributed.get_world_size(group=group) == 1:
-        return input_
-
-    # print(f"group {group}")
-    # # # All-reduce.
-    # output_ = input_.contiguous()
-    # print(f"input_ before {input_}")
-    dist.all_reduce(input_, group=group)
-    # torch.distributed.all_reduce(input_, op=torch.distributed.ReduceOp.SUM)
-    # print(f"input_ after {input_}")
-
-    return input_
 
 
-def _split(input_: torch.Tensor) -> torch.Tensor:
-    """Split the tensor along its last dimension and keep the
-    corresponding slice."""
-    group = get_model_parallel_group()
-
-    # Bypass the function if we are using only 1 GPU.
-    if torch.distributed.get_world_size(group=group) == 1:
-        return input_
-
-    # Split along last dimension.
-    world_size = torch.distributed.get_world_size(group=group)
-    input_list = split_tensor_along_last_dim(input_, world_size)
-
-    # Note: torch.split does not create contiguous tensors by default.
-    rank = torch.distributed.get_rank(group=group)
-    output = input_list[rank].contiguous()
-
-    return output
-
-
-def _gather(input_: torch.Tensor) -> torch.Tensor:
+def _gather(input_: torch.Tensor, group_:torch.distributed.ProcessGroup) -> torch.Tensor:
     """Gather tensors and concatinate along the last dimension."""
-    group = get_model_parallel_group()
+    group = group_
 
     # Bypass the function if we are using only 1 GPU.
     if torch.distributed.get_world_size(group=group) == 1:
